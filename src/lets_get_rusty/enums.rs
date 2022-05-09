055a6c947533fcab5ab2ef81b50f3c39f322b69e
Qwox <qwox@qwox.com>
2022-05-09 23:27:19 +0200

    lgr/enums.rs: warnings cleanup

--- src/lets_get_rusty/enums.rs
+++ src/lets_get_rusty/enums.rs
@@ -22,8 +22,8 @@ struct IpAddress {
 enum Message {
     Quit,                       // -> no Data
     Move { x: i32, y: i32 },    // -> ananymous struct
-    Write(String),              // -> String
-    ChangeColor(i32, i32, i32), // -> 3 int
+    Write(String),              // -> 1 String Tuple
+    ChangeColor(i32, i32, i32), // -> 3 int Tuple
 }
 
 #[allow(unused)]
@@ -37,40 +37,59 @@ impl Message {
 // ------------------
 // Coin Enum
 enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter,
+    Silver,
+    Gold,
+    Custom { value: u64 },  // Custom -> value struct
 }
 
-
-// =====================
-// functions
-fn value_in_cents(coin: Coin) -> u64 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky Penny");
-            1
+fn get_coin_value(coin: &Coin) -> u64 {
+    // coin: borrowed Coin -> *coin: real Coin
+    match *coin {
+        Coin::Silver => 10,
+        Coin::Gold => {
+            println!("Gold!");
+            1000
         },
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Custom { value: val } => val,
+        // Custom Variant with value val (wildcard: _)
+        // -> return val
     }
 }
 
+// Alternative: Method
+impl Coin {
+    fn value(&self) -> u64 {
+        match *self {
+            // Self == Coin
+            Self::Silver => 10,
+            Self::Gold => {
+                println!("Gold!");
+                1000
+            },
+            Self::Custom { value: val } => val,
+        }
+    }
+}
 
 // =====================
 // main
 pub fn run() {
-    // No Data in Enum
+    // --- No Data in Enum
     // let localhost = IpAddress {
     //     kind: IpAddressKind::IPv4,
     //     address: String::from("127.0.0.1"),
     // };
-
-    // Enum with Data
+    // --- Enum with Data
     #[allow(unused_variables)]
     let localhost = IpAddressKind::IPv4(127, 0, 0, 1);
+
+
+    // --- Coins
+    let s_coin = Coin::Silver;
+    let g_coin = Coin::Gold;
+    let my_coin = Coin::Custom { value: 333 };
+    println!("silver: {}; gold: {}; my_coin: {}", get_coin_value(&s_coin), g_coin.value(), get_coin_value(&my_coin) );
+    // Note: g_coin.value() == &g_coin.value()
 
 
     // Option Enum
@@ -93,7 +112,9 @@ pub fn run() {
     let y: Option<i8> = Some(2);
     // let sum = x + y;  // Error: cannot add i8 type to Option<i8> type
     // -> handle all cases:
-    let sum = x + y.unwrap_or(0);
-
+    let _sum = x + y.unwrap_or(0);
+    // Option.unwrap(): get value of Some
+    //      if None: use unwrap_or() default value
+    // _var: unused variable
     
 }
